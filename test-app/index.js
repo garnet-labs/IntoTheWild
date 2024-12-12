5c46b55a6161c09524e45cb772b9671c5c319e35
Farrukh Jadoon <fkjadoon94@gmail.com>
2024-12-12 11:15:32 -0500

    wip

--- test-app/index.js
+++ test-app/index.js
@@ -1,5 +1,4 @@
 const eslinter = require('../iocs/typescript_eslinter_eslint_2.2.9');
-// const prettier = require('../iocs/typescript_eslinter_prettier_1.2.2');
 
 async function runTests() {
     try {
@@ -19,14 +18,6 @@ async function runTests() {
             rules: { 'no-var': 'error' }
         });
         console.log('Eslinter results:', results);
-
-        // Test prettier
-        console.log('\nTesting prettier...');
-        const prettierResult = await prettier.format(testCode, {
-            parser: 'typescript',
-            singleQuote: true
-        });
-        console.log('Prettier results:', prettierResult);
 
     } catch (error) {
         console.error('Runtime test error:', error);
